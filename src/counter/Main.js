331891239e09fac7e50f57a73a2179a0338f2d99
anshusaurav <anshu.saurav@gmail.com>
2020-06-11 17:08:55 +0530

    Counter-App

--- src/counter/Main.js
+++ src/counter/Main.js
@@ -1,5 +1,34 @@
 import React from 'react'
+class Counter extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {count: 0}
+    // this.increment = this.increment.bind(this);
+    // this.decrement = this.decrement.bind(this);
+    // this.reset = this.reset.bind(this);
+  }
 
-export default function App() {
-  return <h1>Hello Counter</h1>
+  increment = () => {
+    this.setState(state => ({count: state.count + 1}))
+  }
+  decrement = () => {
+    this.setState(state => ({count: state.count - 1}))
+  }
+
+  reset = () => {
+    this.setState(state => ({count: 0}))
+  }
+
+  render () {
+    return (
+      <>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <button onClick={this.reset}>Reset</button>
+      </>
+    )
+  }
 }
+
+export default Counter
