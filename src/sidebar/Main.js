3f7043256a581050c7ffaa7240c2d100fb0baf36
anshusaurav <anshu.saurav@gmail.com>
2020-06-11 21:15:04 +0530

    Sidebar app

--- src/sidebar/Main.js
+++ src/sidebar/Main.js
@@ -1,36 +1,53 @@
 import React from 'react'
 
-class SideBar extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {tabSelected: 'Home', sideBarHidden:false}
+class SideBar extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {tabSelected: 'Home', sideBarHidden: true}
   }
-  selectHome = () =>{
-    this.setState(state =>({tabSelected: 'Home'}));
+  selectHome = () => {
+    this.setState(state => ({tabSelected: 'Home'}))
   }
-  selectContact = () =>{
-    this.setState(state =>({tabSelected: 'Contact'}));
+  selectContact = () => {
+    this.setState(state => ({tabSelected: 'Contact'}))
   }
-  selectAbout = () =>{
-    this.setState(state =>({tabSelected: 'About'}));
+  selectAbout = () => {
+    this.setState(state => ({tabSelected: 'About'}))
   }
-  toggleButtons = () =>{
-    this.setState(state =>({sideBarHidden: !this.state.sideBarHidden}));
+  toggleButtons = () => {
+    this.setState(state => ({
+      sideBarHidden: !this.state.sideBarHidden,
+    }))
   }
 
-  render(){
+  render () {
     return (
-        <>
-        <div className={this.state.sideBarHidden?'visible-buttons':'hidden-buttons'} >
-          <button onClick={this.selectHome}>Home</button>
-          <button onClick={this.selectContact}>Contact</button>
-          <button onClick={this.selectAbout}>About</button>          
+      <>
+        <div className={'sidebarCont'}>
+          <div
+            className={this.state.sideBarHidden? 'sideFrame visible-buttons':'sideFrameMerged hidden-buttons'}
+          >
+            <button onClick={this.selectHome}>Home</button>
+            <button onClick={this.selectContact}>
+              Contact
+            </button>
+            <button onClick={this.selectAbout}>About</button>
+          </div>
+          <div className={'mainFrame'}>
+            <p className='mainText textCenter'>
+              {this.state.tabSelected === 'Home'
+                ? 'This is Home'
+                : this.state.tabSelected === 'Contact'
+                ? 'This is Contact'
+                : 'This is About'}
+            </p>
+          </div>
+          <button className='switchBtn' onClick={this.toggleButtons}>
+            {this.state.sideBarHidden ? 'Hide' : 'Show'}
+          </button>
         </div>
-          <p>{this.state.tabSelected==='Home'?'This is Home':this.state.tabSelected==='Contact'?'This is Contact':'This is About' }</p>
-          <button onClick={this.toggleButtons}>{this.state.sideBarHidden?'Show':'Hide'}</button>
-        </>
-
-    );
+      </>
+    )
   }
 }
-export default SideBar;
+export default SideBar
